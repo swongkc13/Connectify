6179521e724f3aabc2f62ceb3d0d7943fefc8bf7
swongkc13 <swongkc13@gmail.com>
2024-03-22 20:50:05 +0100

    Progress

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,28 +1,62 @@
-import { createContext, useContext, useEffect, useState} from 'react'
+import { IUser } from "@/types";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const INITIAL_USER = {
-    id: '',
-    name: '',
-    email: '',
-    imageUrl: '',
-    bio: '',
-}
+  id: "",
+  name: "",
+  username: "",
+  email: "",
+  imageUrl: "",
+  bio: "",
+};
 
 const INITIAL_STATE = {
-    user: INITIAL_USER,
-    isLoaing: false,
-    isAuthenticated: false,
-    setUser: () => (),
-    setIsAuthenticate: () => (),
-    checkAuthUser: async () => false as boolean,
-}
+  user: INITIAL_USER,
+  isLoading: false,
+  isAuthenticated: false,
+  setUser: () => {},
+  setIsAuthenticated: () => {},
+  checkAuthUser: async () => false as boolean,
+};
+
+//Imported from Github
+type IContextType = {
+  user: IUser;
+  isLoading: boolean;
+  setUser: React.Dispatch<React.SetStateAction<IUser>>;
+  isAuthenticated: boolean;
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+  checkAuthUser: () => Promise<boolean>;
+};
 
 const AuthContext = createContext<IContextType>(INITIAL_STATE);
 
-const AuthContext = () => {
-  return (
-    <div>AuthContext</div>
-  )
-}
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<IUser>(INITIAL_USER);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-export default AuthContext
+  const checkAuthUser = async () => {
+    try {
+      const currentAccount = await getCurrentUser();
+    } catch (error) {
+      console.log(error);
+      return false;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const value = {
+    user,
+    setUser,
+    isLoading,
+    IsAuthenticated,
+    SetIsAuthenticated,
+    checkAuthUser,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
