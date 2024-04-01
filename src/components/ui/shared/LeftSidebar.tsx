2b1ec482c1006369df654956535eb1d7f1d6baee
swongkc13 <swongkc13@gmail.com>
2024-04-01 23:12:06 +0200

    Progress

--- src/components/ui/shared/LeftSidebar.tsx
+++ src/components/ui/shared/LeftSidebar.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LeftSidebar = () => {
   return (
-    <div>LeftSidebar</div>
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+      <Link to="/" className="flex gap-3 items-center">
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={170}
+            height={36}
+          />
+        </Link>
+      </div>
+    </nav>
   )
 }
 
