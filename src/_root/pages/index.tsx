087fcd1b23ab0c62ee0757969f58ab24f259e09d
swongkc13 <swongkc13@gmail.com>
2024-04-07 18:01:19 +0200

    Progress

--- src/_root/pages/index.tsx
+++ src/_root/pages/index.tsx
@@ -1,3 +1,12 @@
-//import all pages
+//import all pages. Exporting pages so we can import them in other files.
 
 export { default as Home } from './Home';
+export { default as Explore } from './Explore';
+export { default as Saved } from './Saved';
+export { default as CreatePost } from './CreatePost';
+export { default as Profile } from './Profile';
+export { default as UpdateProfile } from './UpdateProfile';
+export { default as EditPost } from './EditPost';
+export { default as PostDetails } from './PostDetails';
+export { default as AllUsers } from './AllUsers';
+export { default as LikedPosts } from './LikedPosts';
