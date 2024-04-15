8dc22dab9e226ab0a269715f63a0b5c705c675d4
swongkc13 <swongkc13@gmail.com>
2024-04-15 18:03:20 +0200

    Progress

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 
 const FileUploader = () => {
-  return (
-    <div>FileUploader</div>
-  )
-}
+  const [fileUrl, setFileUrl] = useState('')
 
-export default FileUploader
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()} className="flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer">
+      <input {...getInputProps()} className="cursor-pointer" />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
+};
+
+export default FileUploader;
--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,6 +1,6 @@
 //import { useEffect } from 'react'
 import { Link, NavLink, useNavigate, useLocation } from 'react-router-dom'
-import { Button } from '../button'
+import { Button } from '../ui/button'
 import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
 import { useUserContext } from '@/context/AuthContext'
 import { sidebarLinks } from '@/constants'
