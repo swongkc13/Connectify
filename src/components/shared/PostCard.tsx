84b385a6af7f3ec0154601a0c8daec12291f53ff
swongkc13 <swongkc13@gmail.com>
2024-04-22 11:34:14 +0200

    Progress

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -1,3 +1,4 @@
+import { multiFormatDateString } from "@/lib/utils";
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
 
@@ -19,20 +20,32 @@ const PostCard = ({ post }: PostCardProps) => {
               alt="creator"
               className="rounded-full w-12 lg:h-12"
             />
-            <div className="flex flex-col">
-              <p className="base-medium lg:body-bold text-light-1">{post.creator.name}</p>
-              <div className="flex-center gap-2 text-light-3">
-                <p className="subtle-semibold lg:small-regular">
-                {post.$createdAt}
-                </p>
-                -
-                <p className="subtle-semibold lg:small-regular">
-                    {post.location}
-                </p>
-              </div>
-            </div>
           </Link>
+
+          <div className="flex flex-col">
+            <p className="base-medium lg:body-bold text-light-1">
+              {post.creator.name}
+            </p>
+            <div className="flex-center gap-2 text-light-3">
+              <p className="subtle-semibold lg:small-regular">
+                {multiFormatDateString(post.$createdAt)}
+              </p>
+              -
+              <p className="subtle-semibold lg:small-regular">
+                {post.location}
+              </p>
+            </div>
+          </div>
         </div>
+
+        <Link to={`/update-post/${post.$id}`}>
+          <img
+           src="/assets/icons/edit.svg"
+           alt="edit"
+           width={20}
+           height={20}
+          />
+        </Link>
       </div>
     </div>
   );
